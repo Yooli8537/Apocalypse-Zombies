7299b92eb6c377ec055cadc1349ebb9a697816d0
Yooli8537 <Yooli8537@gmx.ch>
2026-06-30 14:21:52 +0200

    Wrote Logic for InfectedEffect

--- src/main/java/com/y_apocalypse_zombies/InfectedEffect.java
+++ src/main/java/com/y_apocalypse_zombies/InfectedEffect.java
@@ -1,4 +1,24 @@
 package com.y_apocalypse_zombies;
 
-public class InfectedEffect {
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
+
+public class InfectedEffect extends MobEffect {
+    public InfectedEffect() {
+        super(MobEffectCategory.HARMFUL, 0x8dbb32); // 0x = Marks Hexadecimal
+    }
+
+    @Override
+    public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
+        // Only applies damage every 30 Ticks
+        return duration % 30 == 0;
+    }
+
+    @Override
+    public boolean applyEffectTick(ServerLevel level, LivingEntity entity, int amplifier) {
+        entity.hurtServer(level, level.damageSources().magic(), 2.0F + amplifier);
+        return super.applyEffectTick(level, entity, amplifier);
+    }
 }
